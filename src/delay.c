d8da33f0372dcd1e5aecf67c40a030e75746e38a
Aram Verstegen <aram.verstegen@gmail.com>
2015-08-10 03:29:17 +0200

    Added a simple delay function

--- src/delay.c
+++ src/delay.c
@@ -1,20 +1,12 @@
 #include "delay.h"
 
 // call: 6 cycles
-void delay_20_cycles(void){
-    // nops: 1 cycle each
-    __asm__("nop\n"
-            "nop\n"
-            "nop\n");
-    // ret: 11 cycles
-}
-
 void _delay_us(uint32_t d){
     uint32_t i = 0;
     // fudged number: adjust for loop accounting
-    for(i = 0; i < (d/TICKS_PER_US/20)-1; i++){
-        delay_20_cycles();
+    for(i = 0; i < (d/TICKS_PER_US/2); i++){
     }
+    // ret: 11 cycles
 }
 
 void _delay_ms(uint32_t d){
